e8994f9cf1bfe8148d164646c442bbe1b1701c7e
chasebls <tylerchasebrock@gmail.com>
2021-08-13 15:22:09 -0600

    changed from number to bigint to preserve accuracy

--- scripts/maths.js
+++ scripts/maths.js
@@ -4,15 +4,20 @@ function enterNum() {
     const main = document.getElementById("main"); // assign main to a variable to append the div
     main.textContent = '';
 
-    let userInput = 0; // initialize (or re-initialize) the input
-    userInput = parseInt(document.getElementById("numberToCalc").value); // get the number from input
-    console.log("enterNum() finished; current number is: " + userInput);
+    let userInput = 0n; // initialize (or re-initialize) the input
 
-    if (typeof (userInput) != 'number' || isNaN(userInput)) {
+    try {
+        userInput = BigInt(document.getElementById("numberToCalc").value); // get the number from input
+    } catch (error) {
         console.warn("Error! This is not a (valid) number. Type of input: " + typeof (userInput) +
             "Using 7 instead, because why not?");
-        window.alert("This is not a number. Please use a real number next time. For now, try 7. 7 is a real number.");
-        collatz(7);
+        console.log("This is not a number. Please use a real number next time. " +
+            "Try 7. 7 is a real number.");
+        return;
+    }
+    console.log("enterNum() finished; current number is: " + userInput);
+
+    if (typeof (userInput) != 'bigint' || userInput <= 0) {
         return;
     }
 
@@ -34,9 +39,9 @@ function createDiv(num, symbol) {
 
     // check if the symbol is + or - and change the color accordingly
     if (symbol == "(+)") {
-        newDiv.style.color = "#9295DD";
+        newDiv.style.color = "#268EBA";
     } else {
-        newDiv.style.color = "#FF3333";
+        newDiv.style.color = "#FF8C42";
     }
 
     newDiv.appendChild(newDivText); // append the p element to the div
@@ -46,29 +51,47 @@ function createDiv(num, symbol) {
     console.log("createDiv() finished; appended a new div with number: " + num);
 }
 
+function updateCounter(counter) {
+    const main = document.getElementById("main"); // assign main to a variable to append the div
+    let newDiv = document.createElement("div"); // create a new div
+    let newDivText = document.createElement("p"); // create a new paragraph
+
+    // set the text of the p to the counter
+    newDivText.innerHTML = ("Nodes in this chain: " + counter);
+    newDiv.appendChild(newDivText);
+    newDiv.classList.add("numberNode");
+    main.prepend(newDiv);
+    console.log("OK");
+}
+
 function collatz(num) {
     console.log("This will only show once... if not a loop.");
+    let counter = 0; // total of how many numbers in this chain
     let lastNum = 0; // to determine if it's larger or smaller than the last number
-    let symbol = "";
+    let symbol = ""; // to display if the number went up or down
     while (true) {
 
-        if (num > lastNum) {
+        if (num > lastNum) { // if greater than the last number, return a plus symbol
             symbol = "(+)";
         } else {
-            symbol = "(-)";
+            symbol = "(-)"; // if less than the last number, return a minus symbol
         }
+
+        counter++;
+        console.log("Numbers crossed: " + counter);
         lastNum = num;
 
-        createDiv(num, symbol);
+        createDiv(num, symbol); // create a div with this number and its symbol
 
-        if (num == 1) {
+        if (num == 1n) {
             console.log("Found a one (1). It's all over!");
+            updateCounter(counter)
             return; // if the number is 1, the loop is complete
         };
-        if (num % 2 == 1) { // if the number is odd, perform this operation
-            num = ((num * 3) + 1)
+        if (num % 2n == 1n) { // if the number is odd, perform this operation
+            num = ((num * 3n) + 1n)
         } else {
-            num /= 2;
+            num /= 2n;
         };
     };
 };
