8d6316b34e3ce2ad19ccdddf5de74e4ad4eefc47
chasebls <tylerchasebrock@gmail.com>
2021-08-13 13:37:55 -0600

    trying to commit

--- scripts/maths.js
+++ scripts/maths.js
@@ -4,6 +4,7 @@ function enterNum() {
     const main = document.getElementById("main"); // assign main to a variable to append the div
     main.textContent = '';
 
+    //test
     let userInput = 0; // initialize (or re-initialize) the input
     userInput = parseInt(document.getElementById("numberToCalc").value); // get the number from input
     console.log("enterNum() finished; current number is: " + userInput);
